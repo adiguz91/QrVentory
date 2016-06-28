d0012d9292350946d9e117de20e496416012f133
Adrian Guzikowski <adiguzik@gmail.com>
2016-06-28 10:04:08 +0200

    Added FAB menu into ItemActitiy with onclick base construct

--- app/src/main/java/com/example/is2/test2qrventory/ItemActivity.java
+++ app/src/main/java/com/example/is2/test2qrventory/ItemActivity.java
@@ -2,12 +2,14 @@ package com.example.is2.test2qrventory;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.epson.lwprint.sdk.nsd.dns.DNSRecord;
 import com.example.is2.test2qrventory.model.Item;
 import com.example.is2.test2qrventory.model.User;
+import com.github.clans.fab.FloatingActionButton;
 
 public class ItemActivity extends AppCompatActivity {
 
@@ -16,6 +18,9 @@ public class ItemActivity extends AppCompatActivity {
     private TextView textViewTitle;
     private TextView textViewDescription;
 
+    private FloatingActionButton fab_update;
+    private FloatingActionButton fab_delete;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +28,19 @@ public class ItemActivity extends AppCompatActivity {
 
         textViewTitle = (TextView) findViewById(R.id.textViewTitle);
         textViewDescription = (TextView) findViewById(R.id.textViewDescription);
+
+        fab_update = (FloatingActionButton) findViewById(R.id.menu_item_update);
+        fab_delete = (FloatingActionButton) findViewById(R.id.menu_item_delete);
+
+        fab_update.setOnClickListener(onUpdateHandler);
+        fab_delete.setOnClickListener(onDeleteHandler);
+
+        /*fab_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String test = "test";
+            }
+        });*/
 
         // enable UP navigation
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -34,4 +52,18 @@ public class ItemActivity extends AppCompatActivity {
         textViewTitle.setText(single_item.getName());
         textViewDescription.setText(single_item.getDescription());
     }
+
+    View.OnClickListener onDeleteHandler = new View.OnClickListener() {
+        public void onClick(View v) {
+            String test = "delete";
+        }
+    };
+
+    View.OnClickListener onUpdateHandler = new View.OnClickListener() {
+        public void onClick(View v) {
+            // it was the 1st button
+            String test = "update";
+        }
+    };
+
 }
